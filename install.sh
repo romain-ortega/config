daf3df0522a1023ebf6734660a7eb5eb22e6188d
Romain ORTEGA <romain.ortega@protonmail.com>
2016-11-30 14:38:57 +0100

    Improve installation script

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 npm -g install instant-markdown-d && npm install -g typescript
+brew install ctags
+
 # Check if Vundle exist, else install it
 if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
