1d6718a7ac0f9ae8e94710ea8da454d26040e767
Romain Ortega <romain.ortega@protonmail.com>
2017-01-08 17:18:56 +0100

    Make installation easier

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-git clone https://github.com/romain-ortega/config.git /tmp/config && (cd /tmp/config && mv ~/.vimrc ~/.vimrc.old && mv ~/.vim/autoload ~/.vim/autoload.old && mv ~/.vim/bundle/ ~/.vim/bundle.old/ && cp .vimrc ~/.vimrc)
+git clone https://github.com/romain-ortega/config.git /tmp/config && (cd /tmp/config && mv ~/.vimrc ~/.vimrc.old && mv ~/.vim/autoload ~/.vim/autoload.old && mv ~/.vim/bundle/ ~/.vim/bundle.old/)
 
-mkdir -p ~/.vim/backups
-brew install ctags node
-mkdir ~/.vim/colors && cp colors/Ronokai.vim ~/.vim/colors
+mkdir -p $HOME/.vim/backups
+cp .vimrc $HOME/.vimrc
+mkdir $HOME/.vim/colors && cp colors/Ronokai.vim
 npm -g install instant-markdown-d && npm install -g typescript
+brew install ctags
 mkdir $HOME/.vim/tmp
 
 # Check if Vundle exist, else install it
-if [[ ! -d $HOME/.vim/autoload/plug.vim ]]; then
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [[ ! -d $HOME/.vim/bundle/Vundle.vim ]]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 fi
 vim +PluginInstall! +qall
